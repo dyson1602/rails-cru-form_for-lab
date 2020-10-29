504a76d00f71782ce2f1efb28d1f8e5a35c29359
dyson1602 <cclark000@citymail.cuny.edu>
2020-10-28 21:01:19 -0400

    lol these tests are ridiculous

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+  # get '/songs', to: 'songs#index', as: 'songs'
+  # get '/songs', to: 'songs#index', as: 'songs'
+  # get '/songs', to: 'songs#index', as: 'songs'
+  # get '/songs', to: 'songs#index', as: 'songs'
+
+resources :songs, :artists, :genres
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+Song.destroy_all
+Artist.destroy_all
+Genre.destroy_all
+
+artist1 = Artist.create(name: Faker::Name.name, bio: Faker::Lorem.paragraph(sentence_count:3))
+artist2 = Artist.create(name: Faker::Name.name, bio: Faker::Lorem.paragraph(sentence_count:3))
+artist3 = Artist.create(name: Faker::Name.name, bio: Faker::Lorem.paragraph(sentence_count:3))
+
+genre1 = Genre.create(name: "metal")
+genre2 = Genre.create(name: "rock")
+genre3 = Genre.create(name: "rap")
+
+Song.create(name: Faker::Music::PearlJam.song, artist_id: artist1.id, genre_id: genre1.id)
+Song.create(name: Faker::Music::PearlJam.song, artist_id: artist2.id, genre_id: genre2.id)
+Song.create(name: Faker::Music::PearlJam.song, artist_id: artist3.id, genre_id: genre3.id)
